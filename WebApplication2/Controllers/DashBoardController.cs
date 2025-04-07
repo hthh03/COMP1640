26e4de4235f963b6ad34fd57ea56c3aec66e97b0
QuangThang <thangttqgcd210665@fpt.edu.vn>
2025-04-07 20:08:15 +0700

    Resolve conflict

---
+++ WebApplication2/Controllers/DashBoardController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+using WebApplication2.Data;
+
+namespace WebApplication2.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        private readonly AppDbContext _context;
+        public DashBoardController (AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
---
+++ WebApplication2/Controllers/ProfileController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<Users> _userManager;
+        private readonly SignInManager<Users> _signInManager;
+
+        public ProfileController(UserManager<Users> userManager, SignInManager<Users> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Profile()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+                return RedirectToAction("Login", "Account");
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Profile(Users model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+                return RedirectToAction("Login", "Account");
+
+            user.FullName = model.FullName;
+
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+                ViewBag.Message = "Cập nhật thành công!";
+            else
+                ViewBag.Message = "Cập nhật thất bại!";
+
+            return View(user);
+        }
+    }
+}
